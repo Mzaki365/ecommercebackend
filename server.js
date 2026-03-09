b62379d240a7e4d688c40ac8260b65abcf47a79d
Mzaki365 <Muhammadzakip36@gmail.com>
2026-03-09 21:24:55 +0500

    change in server.js

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
+
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-// const dns = require("dns");
+
 const dbConnect = require("./src/config/db");
 const userRoutes = require("./src/routes/userRoutes");
 const productRoutes = require("./src/routes/productRoutes");
 const { notFound, errorHandler } = require("./src/middlewares/errorMiddleware");
 
+// Load environment variables
 dotenv.config();
+
+// Connect database
 dbConnect();
+
 const app = express();
-// dns.setServers(["1.1.1.1","0.0.0.0"])
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-
-// Health check
+// Health check route
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
@@ -30,11 +33,14 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 
-// Error handling middleware (must be last)
+// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
+// Start server
 const PORT = process.env.PORT || 5000;
 
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
